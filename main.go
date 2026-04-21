656d194dc780043ede9f8975d0f8a8c0305781ef
Zhu Xian <zhuxianwin@users.noreply.github.com>
2026-04-21 10:54:52 +0800

    docs: add comment noting personal fork purpose

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 
 func main() {
 	// Run fzf and exit with the appropriate exit code
+	// Personal fork: tracking upstream junegunn/fzf for learning and customization
 	exitCode := fzf.Run(fzf.ParseOptions())
 	os.Exit(exitCode)
 }
